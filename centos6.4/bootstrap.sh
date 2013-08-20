dc9bfa954e291d07a397dfbb9aea4b12c3625cea
Kevin Risden <risdenk@avalonconsult.com>
2013-08-20 16:33:31 -0500

    Removed hosts files and use vagrant-hostmanager plugin

--- centos6.4/bootstrap.sh
+++ centos6.4/bootstrap.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-cp /vagrant/hosts /etc/hosts
-cp /vagrant/resolv.conf /etc/resolv.conf
 yum install ntp -y
 service ntpd start
--- centos6.4/dev-bootstrap.sh
+++ centos6.4/dev-bootstrap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cp /vagrant/hosts /etc/hosts
-cp /vagrant/resolv.conf /etc/resolv.conf
 yum install ntp -y
 service ntpd start
 
